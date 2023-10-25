7a1e559bdc61cf982219c042fec90bb4930a8de8
Chris Compton <daniel.compton@nist.gov>
2023-10-25 13:03:37 -0500

    Updates with basics for ssp.

--- ssp.py
+++ ssp.py
@@ -1,176 +1,98 @@
 ########################################################################
 #%% Work with controls from: https://github.com/usnistgov/csv-synthetic-controls/
 
+import os
 import pandas as pd
-import datetime, json, yaml, os
-import oscalic.schema as oscal
 import tools as t
 
-from pathlib import Path
-from oscalic.system_security_plan import SystemSecurityPlan as SSP
-from oscalic.control import ControlAssembly as Control, StatementAssembly as Statement, ByComponentAssembly as ByComponent
-from oscalic import Template, Helper, Validation
-from xsdata_pydantic.bindings import XmlParser, JsonParser
-
-from pprint import pprint
-
-error_condition = None
-errors = list()
-
 ########################################################################
 
+error_condition             = None
+errors                      = list()
 
+#%% Paths os.getcwd()
+dir_output                  = os.path.join(os.getcwd(),'output')
+dir_template                = os.path.join(os.getcwd(),'templates')
+dir_content                 = os.path.join(os.getcwd(),'content')
 
-#%% Paths
-dir_output = os.path.join(os.getcwd(), 'output')
-dir_template = os.path.join(os.getcwd(), 'template')
-dir_content = os.path.join(os.getcwd(), 'content')
+filename_output             = 'example'
+filename_input              = 'NIST_800-53_Rev5_Simulated.csv'
 
-filename_output = 'SSP.example'
-filename_input = 'NIST_800-53_Rev5_Simulated.csv'
-
-filepath_yaml = f"{dir_output}/{filename_output}.yaml"
-filepath_json = f"{dir_output}/{filename_output}.json"
-filepath_csv = f"{dir_content}/{filename_input}"
+filepath_csv                = os.path.join(dir_content, filename_input)
 
 #%% Static Settings
-ssp_system_name = 'Demonstration System with Synthetic Content'
-ssp_reason = 'This SSP demonstrates exports using only the exports mechanism, and would require SSP model modifications.'
-
-sep = '*'*100
-lb = "\n\n"
-
-########################################################################
-
-
+sep                         = '*'*100
+lb                          = "\n\n"
 
 #%% Setup
-df = pd.read_csv(filepath_csv)
-grouped_df = df.groupby('control_id')
+df_content                  = pd.read_csv(filepath_csv)
+grouped_df                  = df_content.groupby('control_id')
 
-this_system_component_uuid = str(Helper.get_uuid())
-today = datetime.datetime.now()
-today_format = '%Y-%m-%dT00:00:00.0000-04:00'
-today = today.strftime(today_format)
-control_list = list()
+#%% Load all templates
+templates = []
+for root, dirs, files in os.walk(dir_template):
+    for file in files:
+        if file.endswith(".yaml"):
+             templates.append(file)
 
-#%% Start SSP
-ssp_template = 'template.ssp.yaml'
-ssp_data = {
-    'uuid:document':        str(Helper.get_uuid()),
-    'uuid:statement':       str(Helper.get_uuid()),
-    'uuid:component':       this_system_component_uuid, 
-    'uuid:user':            str(Helper.get_uuid()),
-    'uuid:party':           str(Helper.get_uuid()), 
-    'uuid:by-component':    str(Helper.get_uuid()), 
-    'uuid:information-type':str(Helper.get_uuid()), 
+########################################################################
+
+org_metadata = {}
+
+
+org_metadata['csp'] = {
     'version':              '0.0.1',
-    'modified_date':        f"{today}",
-    'ssp_system_name':      ssp_system_name,
-    'ssp_reason':           ssp_reason
+    'ssp_system_name':      'Demonstration System representing a Cloud Service Provider',
+    'ssp_reason':           'This SSP demonstrates prototype modeling for sharing of responsibility.'   
 }
-ssp_content = Template.apply(ssp_template, ssp_data)
-ssp = Helper.from_yaml(SSP, ssp_content)
+
+org_metadata['msp'] = {
+    'version':              '0.0.1',
+    'ssp_system_name':      'Demonstration System representing a Managed Service Provider',
+    'ssp_reason':           'This SSP demonstrates prototype modeling for sharing of responsibility.'   
+}
+
+org_metadata['app'] = {
+    'version':              '0.0.1',
+    'ssp_system_name':      'Demonstration System representing an Application Owner',
+    'ssp_reason':           'This SSP demonstrates prototype modeling for sharing of responsibility.'       
+}
+
+   
+
+#%% Build SSPs
+for ssp_template in templates:
+    current_org                 = ssp_template.split('.')[2:-1][0]
+    filepath_template           = os.path.join(dir_template,ssp_template)
+    filepath_yaml               = os.path.join(dir_output, f"{filename_output}.{'.'.join(ssp_template.split('.')[1:-1])}.yaml")
+    filepath_json               = os.path.join(dir_output, f"{filename_output}.{'.'.join(ssp_template.split('.')[1:-1])}.json")
+
+    print(f"Generating [{current_org}]: {filepath_template}")
+    print(f"YAML: {filepath_yaml}")
+    print(f"JSON: {filepath_json}\n\n")
+
+    metadata = org_metadata[current_org]
+
+    # Build Content
+    print("Building SSP")
+    ssp = t.build_ssp(filepath_template, metadata, grouped_df)
+
+    # Export YAML file
+    print(f"YAML: {filepath_yaml}")
+    t.save_yaml(ssp, filepath_yaml)
+
+    # Export JSON file
+    print(f"JSON: {filepath_json}")
+    t.save_json(ssp, filepath_json)
+
+    print(f"Validate/oscal-cli-1.0.2 ssp validate {filepath_yaml}")
+    print(f"Validate/oscal-cli-1.0.2 ssp validate {filepath_json}")
+
+    print("\n\n")
 
 
 
 ########################################################################
-#%% Build Content
-for control_id, group in grouped_df:
-    # if control_id == 'ac-2':
-        print(control_id.upper())
-
-        statements = list()
-        for index, row in group.iterrows():
-            components = list()
-            
-            # 'implementation-status': {
-            #     'state': row['state'],
-            #     'remarks': ''
-            # }
-
-            provided_uuid = str(Helper.get_uuid())
-            provided_content = [{
-                'uuid': provided_uuid,
-                'description': row['export_provided'],
-
-            }]
-
-            responsibilities_content = [{
-                'uuid': str(Helper.get_uuid()),
-                'provided-uuid': provided_uuid,
-                'description': row['export_responsibility']
-            }]
-
-            satisfied_content = [{
-                'uuid': str(Helper.get_uuid()),
-                'responsibility-uuid': str(Helper.get_uuid()),
-                'description': row['export_responsibility']                 
-            }]
-
-            inherited_content = [{
-                'uuid': str(Helper.get_uuid()),
-                'provided-uuid': provided_uuid,
-                # 'satisfied-uuid': satisfied_uuid,
-                'description': row['export_responsibility']                 
-            }]
-
-            #############################################################
-            # Deprecated
-            # export_content = {
-            #     'provided': provided_content,
-            #     'responsibilities': responsibilities_content
-            # } 
-
-            #############################################################
-            component_content = {
-                'component_uuid': this_system_component_uuid,
-                'uuid': str(Helper.get_uuid()),
-                'description': row['description'],
-                'provided': provided_content,
-                'responsibilities': responsibilities_content,
-                'satisfied': satisfied_content,
-                'inherited':inherited_content
-                # 'export': export_content
-            }
-            component = ByComponent.construct(**component_content)
-            components.append(component)
-
-            #############################################################
-            statement_content = {
-                'statement_id': row['statement_id'],
-                'uuid': str(Helper.get_uuid()),
-                'by_components': components
-            }
-            statement = Statement.construct(**statement_content)
-            statements.append(statement)
-
-        control_content = {
-            'uuid': str(Helper.get_uuid()),
-            'control_id': row['control_id'],
-            'statements': statements
-        }
-
-        try:
-            control = Control.construct(**control_content)
-            output = Helper.to_yaml(control)
-            ssp.system_security_plan.control_implementation.implemented_requirements.append(control)
-        except Validation.OSCALValidationError as e:
-            errors.append(e.json())
-            error_condition = 1
-
-
-
-
-########################################################################
-#%% Export YAML file
-t.save_yaml(ssp,filepath_yaml)
-
-#%% Export JSON file
-t.save_json(ssp,filepath_json)
-
-
-
 # %% Validation
 # Validate/oscal-cli-1.0.2 ssp validate output/SSP.exports.example.a.json >ssp.txt 2>&1
 
--- tools.py
+++ tools.py
@@ -1,10 +1,17 @@
-import os, json
+import datetime, os, json
 
 from pathlib import Path
 from oscalic import Helper
 
 from faker import Faker
 fake_it = Faker()
+
+
+from oscalic.system_security_plan       import SystemSecurityPlan as SSP
+from oscalic.control                    import ControlAssembly as Control
+from oscalic.control                    import StatementAssembly as Statement
+from oscalic.control                    import ByComponentAssembly as ByComponent
+from oscalic                            import Template, Helper, Validation
 
 class Identifier:
     uuid = None
@@ -37,7 +44,6 @@ def save_json(ssp, filepath_json):
 
 
 ########################################################################
-
 # def get_ipsom_provided:
 # source <- target
 # provided <- inherited
@@ -60,6 +66,109 @@ def get_related_uuids(target_name, source_name, source_uuid=None):
     if source_uuid:
         rel.source.uuid = source_uuid
 
-
-
     return rel
+
+
+def build_ssp(filepath_template, metadata, controls):
+
+    this_system_component_uuid  = str(Helper.get_uuid())
+    today                       = datetime.datetime.now()
+    today_format                = '%Y-%m-%dT00:00:00.0000-04:00'
+    today                       = today.strftime(today_format)
+
+    ssp_data = {
+        'uuid:component':       this_system_component_uuid,
+        'uuid:document':        str(Helper.get_uuid()),
+        'uuid:statement':       str(Helper.get_uuid()),
+        'uuid:user':            str(Helper.get_uuid()),
+        'uuid:party':           str(Helper.get_uuid()), 
+        'uuid:by-component':    str(Helper.get_uuid()), 
+        'uuid:information-type':str(Helper.get_uuid()), 
+        'modified_date':        f"{today}",
+    }
+    ssp_data.update(metadata)
+
+    ssp_content = Template.apply(filepath_template, ssp_data)
+    ssp         = Helper.from_yaml(SSP, ssp_content)
+
+    for control_id, group in controls:
+        # if control_id == 'ac-2':
+            # print(control_id.upper())
+
+        statements = list()
+        for index, row in group.iterrows():
+            components = list()
+            
+            # 'implementation-status': {
+            #     'state': row['state'],
+            #     'remarks': ''
+            # }
+
+            provided_uuid = str(Helper.get_uuid())
+            provided_content = [{
+                'uuid': provided_uuid,
+                'description': row['export_provided'],
+
+            }]
+
+            responsibilities_content = [{
+                'uuid': str(Helper.get_uuid()),
+                'provided-uuid': provided_uuid,
+                'description': row['export_responsibility']
+            }]
+
+            satisfied_content = [{
+                'uuid': str(Helper.get_uuid()),
+                'responsibility-uuid': str(Helper.get_uuid()),
+                'description': row['export_responsibility']                 
+            }]
+
+            inherited_content = [{
+                'uuid': str(Helper.get_uuid()),
+                'provided-uuid': provided_uuid,
+                # 'satisfied-uuid': satisfied_uuid,
+                'description': row['export_responsibility']                 
+            }]
+
+            #############################################################
+            # Deprecated
+            # export_content = {
+            #     'provided': provided_content,
+            #     'responsibilities': responsibilities_content
+            # } 
+
+            #############################################################
+            component_content = {
+                'component_uuid': this_system_component_uuid,
+                'uuid': str(Helper.get_uuid()),
+                'description': row['description'],
+                'provided': provided_content,
+                'responsibilities': responsibilities_content,
+                'satisfied': satisfied_content,
+                'inherited':inherited_content
+                # 'export': export_content
+            }
+            component = ByComponent.construct(**component_content)
+            components.append(component)
+
+            #############################################################
+            statement_content = {
+                'statement_id': row['statement_id'],
+                'uuid': str(Helper.get_uuid()),
+                'by_components': components
+            }
+            statement = Statement.construct(**statement_content)
+            statements.append(statement)
+
+        control_content = {
+            'uuid': str(Helper.get_uuid()),
+            'control_id': row['control_id'],
+            'statements': statements
+        }
+
+
+        control = Control.construct(**control_content)
+        ssp.system_security_plan.control_implementation.implemented_requirements.append(control)
+
+
+    return ssp
